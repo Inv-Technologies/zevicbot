aa9257c06077dbd1744464b0fb1108b2cde8fa23
Inv Technologies <37981347+Inv-Technologies@users.noreply.github.com>
2018-05-04 21:03:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -133,13 +133,7 @@ async def invite():
 async def supportserver():
     await bot.say("Support server: https://discord.gg/YAZNjbe")
 
-if __name__ == "__main__":
- for extension in startup_extensions:
-  try:
-   bot.load_extension(extension)
-  except Exception as e:
-   exc = '{}: {}'.format(type(e).__name__, e)
-   print('Failed to load extension {}\n{}'.format(extension, exc))
+
   
 
 @bot.command()
